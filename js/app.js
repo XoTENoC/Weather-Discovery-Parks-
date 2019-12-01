26d0aa52f362df9dd4af630a671c5cdc94c40390
Nathaniel <changipro.nathaniel@gmail.com>
2019-12-01 12:24:30 +1030

    Adding Dark theme

--- js/app.js
+++ js/app.js
@@ -93,3 +93,59 @@ function showTime() {
 }
 
 showTime();
+
+//menu
+
+// This is a general function that could take functionality from many places.
+// All it does is toggle a class on or off, like so:
+// 01: Does "this" target HTML element have "that" class?
+// 02: If it does, remove that class.
+// 03: If it doesn't, add that class.
+// 04: End function.
+function toggleClass(targetElement, addedClass) {
+    if (targetElement.classList.contains(addedClass)) {
+        targetElement.classList.remove(addedClass);
+    } else {
+        targetElement.classList.add(addedClass);
+    }
+};
+
+// This is the function we add to our menu button to enable its click functionality
+document.querySelector('.menu-btn').addEventListener('click', function() {
+
+    // This toggleClass is for added 'menu--open' to our menu HTML element. This will open the menu.
+    toggleClass(document.querySelector('.menu'), 'menu--open');
+
+    // This toggleClass is for adding '.menu-btn--on' to our menu button HTML element. This creates the animation of the hamburger/hotdog icon to the close icon.
+    toggleClass(document.querySelector('.menu-btn'), 'menu-btn--on');
+});
+
+//theme switcher
+
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+var body = document.body;
+
+if (currentTheme) {
+    body.classList.add(currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        body.classList.add('dark');
+        body.classList.remove('light');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        body.classList.add('light');
+        body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
